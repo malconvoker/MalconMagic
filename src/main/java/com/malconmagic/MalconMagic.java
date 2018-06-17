2959c503de4adb1f396b88f09ecf8d9d1ad565d3
Malconvoker <40331231+malconvoker@users.noreply.github.com>
2018-06-17 13:27:01 +0300

    Forge files and initial mod version

---
+++ src/main/java/com/malconmagic/MalconMagic.java
@@ -0,0 +1,32 @@
+package com.malconmagic;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = MalconMagic.MODID, name = MalconMagic.NAME, version = MalconMagic.VERSION, dependencies = MalconMagic.DEPENDENCIES)
+public class MalconMagic
+{
+    public static final String MODID = "malconmagic";
+    public static final String NAME = "MalconMagic";
+    public static final String VERSION = "1.0";
+	public static final String DEPENDENCIES = "required-after:bloodmagic;required-after:biomesoplenty";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // forge example code
+		// (at start) import net.minecraft.init.Blocks;
+        // logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
